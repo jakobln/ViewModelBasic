1ca8824e88b98c79c50a101add6c00a22f71e376
jakobln <62882745+jakobln@users.noreply.github.com>
2020-04-29 11:05:40 +0200

    Initial commit

---
+++ Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModelBasic
+{
+    public class Student
+    {
+        private string _navn;
+
+        public string Navn 
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+    }
+}
---
+++ StudentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModelBasic
+{
+    public class StudentViewModel
+    {
+        public Student Student { get; set; }
+
+        public StudentViewModel()
+        {
+            Student = new Student() { Navn = "Jakob" };
+        }
+    }
+}
